c2e27173f75330defe845e6c0c13403b812e3028
Lukas Olson <olson.lukas@gmail.com>
2020-03-17 17:05:29 -0700

    tmp branch from mac

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ var server = new Hapi.Server();
 server.connection({
 	host: "localhost",
 	port: 3000,
-	tls: {
-		key: readFile("/path/to/server.key", "utf8"),
-		cert: readFile("/path/to/server.crt", "utf8")
-	}
 });
 
 server.route({
 	method: '*',
-	path: "/proxy/{path*}",
+	path: "/{path*}",
 	handler: {
 		proxy: {
 			mapUri: function (request, callback) {
